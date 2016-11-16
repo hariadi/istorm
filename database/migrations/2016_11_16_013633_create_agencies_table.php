1afcf215d89158219de54fb1b470294b87cfa3b1
Hariadi Hinta <diperakui@yahoo.com>
2016-11-16 11:39:56 +0800

    Initialized i-STORM schema and command generator

---
+++ database/migrations/2016_11_16_013633_create_agencies_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAgenciesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('agencies', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('approver_id')->unsigned()->nullable();
+            $table->string('name');
+            $table->mediumText('description');
+            $table->mediumText('address');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('agencies');
+    }
+}
---
+++ database/migrations/2016_11_16_015317_add_istorm_relations.php
@@ -0,0 +1,97 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddIstormRelations extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('agencies', function(Blueprint $table) {
+			$table->foreign('approver_id')
+				->references('id')
+				->on('users');
+		});
+
+		Schema::table('users', function(Blueprint $table) {
+			$table->foreign('agency_id')
+				->references('id')
+				->on('agencies');
+		});
+
+		Schema::table('products', function(Blueprint $table) {
+			$table->foreign('user_id')
+				->references('id')
+				->on('users')
+				->onDelete('cascade');
+
+			$table->foreign('type_id')
+				->references('id')
+				->on('types');
+		});
+
+		Schema::table('orders', function(Blueprint $table) {
+
+			$table->foreign('approver_id')
+				->references('id')
+				->on('users');
+
+			$table->foreign('user_id')
+				->references('id')
+				->on('users')
+				->onDelete('cascade');
+
+			$table->foreign('product_id')
+				->references('id')
+				->on('products')
+				->onDelete('cascade');
+		});
+
+		Schema::table('detail_product', function(Blueprint $table) {
+
+			$table->foreign('product_id')
+				->references('id')
+				->on('products')
+				->onDelete('cascade');
+
+			$table->foreign('detail_id')
+				->references('id')
+				->on('details')
+				->onDelete('cascade');
+		});
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function(Blueprint $table) {
+			$table->dropForeign('users_agency_id_foreign');
+		});
+
+		Schema::table('orders', function(Blueprint $table) {
+			$table->dropForeign('orders_approver_id_foreign');
+			$table->dropForeign('orders_user_id_foreign');
+			$table->dropForeign('orders_product_id_foreign');
+		});
+
+		Schema::table('products', function(Blueprint $table) {
+			$table->dropForeign('products_user_id_foreign');
+			$table->dropForeign('products_type_id_foreign');
+		});
+
+		Schema::table('detail_product', function(Blueprint $table) {
+			$table->dropForeign('detail_product_product_id_foreign');
+			$table->dropForeign('detail_product_detail_id_foreign');
+		});
+    }
+}
